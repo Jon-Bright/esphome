1fbd2a480fa5d7be6cf39657ddab6132b0d7ba34
Jon Bright <jon@siliconcircus.com>
2025-03-01 10:33:47 +0100

    Implement dali_bus config dumping.

--- esphome/components/dali_bus/dali_bus.cpp
+++ esphome/components/dali_bus/dali_bus.cpp
@@ -1,13 +1,21 @@
 #include "esphome/core/gpio.h"
+#include "esphome/core/log.h"
 
 #include "dali_bus.h"
 
 namespace esphome {
 namespace dali_bus {
 
+static const char *const TAG = "dali_bus";
+
 void DALIBusComponent::setup() {}
 
-void DALIBusComponent::dump_config() {}
+void DALIBusComponent::dump_config() {
+  ESP_LOGCONFIG(TAG, "dali_bus:");
+  LOG_PIN("  DALI out: ", this->out_pin_);
+  LOG_PIN("  DALI in: ", this->in_pin_);
+  ESP_LOGCONFIG(TAG, "  Scan: ", YESNO(this->scan_));
+}
 
 }  // namespace dali_bus
 }  // namespace esphome
